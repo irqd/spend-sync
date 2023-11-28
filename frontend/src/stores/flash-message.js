5b137dc05492d738b96b95c38699bab5b715cf55
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-11-28 23:18:13 +0800

    added type check for flash messages

--- frontend/src/stores/flash-message.js
+++ frontend/src/stores/flash-message.js
@@ -1,5 +1,12 @@
 import { defineStore } from "pinia";
 
+const FlashMessageTypes = {
+   INFO: 'info',
+   SUCCESS: 'success',
+   WARNING: 'warning',
+   DANGER: 'danger',
+};
+
 export const useFlashMessageStore = defineStore("flashMessage", {
    state: () => ({
       flashType: null, // success, error, warning, info
@@ -13,8 +20,16 @@ export const useFlashMessageStore = defineStore("flashMessage", {
 
    actions: {
       setFlashMessage(type, message) {
-         this.flashType = type;
-         this.flashMessage = message;
+         try {
+            if (!Object.values(FlashMessageTypes).includes(type)) {
+               throw new Error('Invalid flash message type.');
+            }
+
+            this.flashType = type;
+            this.flashMessage = message;
+         } catch (error) {
+            console.error(error.message);
+         }
       },
 
       clearFlashMessage() {

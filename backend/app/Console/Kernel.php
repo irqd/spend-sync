91c2071aedf9f1ab76ca5b705ea935c4d0ca18db
Ivan Rey Deposoy <ivanrey.deposoy@gmail.com>
2024-10-07 19:36:29 +0800

    basic dockerize

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        // TODO: Add your custom commands here
+        // FIXME: Remove the example command
     }
 
     /**
